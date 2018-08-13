9a22894b96aed31c6fcc8313b9add3c816d37c65
Lucas11-96 <Lucas11-96@users.noreply.github.com>
2018-08-13 23:10:58 +0200

    new Comment

--- SRS-Core/src/SRS.java
+++ SRS-Core/src/SRS.java
@@ -6,6 +6,7 @@ public class SRS {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("DC");
+        System.out.println("new branch");
     }
     
 }
